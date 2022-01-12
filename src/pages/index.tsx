e6b1158adb7a70358791a112d062c65198a2b058
Rhayana Barbosa <rhayanabarbosa@gmail.com>
2022-01-12 13:55:05 -0300

    create home page and buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import styles from './home.module.scss';
+import { SubscribeButton } from "../components/SubscribeButton";
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1>Hello</h1>
+    
+    <main className={styles.contentContainer}>
+      <section className={styles.heroSection}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 monthly</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
     </>
   )
 }
