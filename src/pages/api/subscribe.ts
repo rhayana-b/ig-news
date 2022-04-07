a67acd071b4731b0d7af623c645e28d167ec4cea
Rhayana Barbosa <rhayanabarbosa@gmail.com>
2022-04-07 15:15:24 -0300

    start integrate subscription stripe

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from 'next-auth'
+import { stripe } from "../../services/stripe"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req })
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+      //metadata
+    })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: process.env.API_ID_PRICE, quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: 'http://localhost:3000/posts',
+      cancel_url: 'http://localhost:3000/'
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method is not allowed')
+  }
+}
