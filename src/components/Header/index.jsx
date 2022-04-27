3363479ca202f6d9c77f3390e69904208d725e97
Rhayana Barbosa <rhayanabarbosa@gmail.com>
2022-04-27 13:51:56 -0300

    adjust Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,29 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo ig news" />
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="ig.news" />
+          </a>
+        </Link>
         <nav>
-          <a href="/" className={styles.active}>Home</a>
-          <a href="/posts">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
+
         <SignInButton />
       </div>
     </header>
-  )
+  );
 }
